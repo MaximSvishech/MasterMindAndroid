f2b143d51d2dcc87da94089dfa82c30538c963ca
Boris Martzinovsky <b@Boriss-MacBook-Pro-2.local>
2022-10-21 13:59:02 +0300

    add comments

--- app/src/main/java/com/example/bullsandcows/Bar.java
+++ app/src/main/java/com/example/bullsandcows/Bar.java
@@ -1,6 +1,14 @@
 package com.example.bullsandcows;
 import android.view.View;
 
+/*
+A class for bars used in bar chart layout.
+Each bar holds 3 views:
+namwView will hold the bar's name
+labelView will hold the bar's value
+barView will hold the bar itself (bar width will be set according to value)
+ */
+
 
 public class Bar {
     private View nameView;
@@ -8,13 +16,15 @@ public class Bar {
     private View labelView;
     private Double value;
 
-
+    // constructor
     public Bar(View nameView, View barView, View labelView, Double value) {
         this.nameView = nameView;
         this.barView = barView;
         this.labelView = labelView;
         this.value = value;
     }
+
+    // getters and setters
 
     public View getNameView() {
         return nameView;
--- app/src/main/java/com/example/bullsandcows/utils/SoundFXPoolManager.java
+++ app/src/main/java/com/example/bullsandcows/utils/SoundFXPoolManager.java
@@ -1,5 +1,15 @@
 package com.example.bullsandcows.utils;
 
+/*
+ Manager class for sound effects SoundPool. For long audio clips (like background music) a MediaPlayer
+ is generally used, but for short clips (like sound effects) SoundPool is a more efficient option.
+ Uses a variation on the Singleton pattern.
+ Sounds loading is separated from sounds playing in order to allow for preloading
+ in the main activity.
+
+
+*/
+
 import android.content.Context;
 import android.media.AudioAttributes;
 import android.media.SoundPool;
@@ -12,14 +22,14 @@ public class SoundFXPoolManager {
     private static SoundFXPoolManager Instance;
     private static SoundPool soundPool;
 
-    private boolean isLoaded;
+    private static boolean isLoaded = false;
     private int[] soundResourceIDs;
     private static HashMap<Integer, Integer> ResourceIdToLoadedSoundIDs = new HashMap<>();
 
     private Context context;
 
 
-    public static void instantiate(Context context) {
+    public static void instantiate(Context context) { //instantiate Singleton
         if (Instance == null)
             Instance = new SoundFXPoolManager(context);
     }
@@ -34,29 +44,30 @@ public class SoundFXPoolManager {
         AudioAttributes audioAttrib = new AudioAttributes.Builder()
                 .setUsage(AudioAttributes.USAGE_ALARM)
                 .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                .build();
+                .build(); //various audio playback attributes
         soundPool = new SoundPool.Builder().
                 setAudioAttributes(audioAttrib).
                 setMaxStreams(3).build();
 
 
         soundResourceIDs = new int[]{R.raw.pop, R.raw.long_pop, R.raw.tada, R.raw.negative_beeps,
-                R.raw.sweep, R.raw.transition_short,  R.raw.swoosh };
+                R.raw.sweep, R.raw.transition_short,  R.raw.swoosh }; //sounds to be used
 
         for (int resourceID : soundResourceIDs)
             ResourceIdToLoadedSoundIDs.put(resourceID,
-                    soundPool.load(context, resourceID , 1));
+                    soundPool.load(context, resourceID , 1)); //preload sounds
 
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
             public void onLoadComplete(SoundPool soundPool, int mySoundId, int status) {
-                isLoaded = true;
+                isLoaded = true; //sounds are now loaded and playable
             }
         });
     }
 
     public static void playSound(int soundResourceID) {
         Integer soundID = ResourceIdToLoadedSoundIDs.get(soundResourceID);
-        soundPool.play(soundID, 1, 1, 0, 0, 1);
+        if (isLoaded)
+            soundPool.play(soundID, 1, 1, 0, 0, 1);
     }
 }
